1b01f348e5aecfdfa8463bf85ab02e025bf691de
EnzoTaniolo <101295426+EnzoTaniolo@users.noreply.github.com>
2025-12-09 13:45:00 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 function Home() {
-    return <div>  
-                <h1>Ana, Eu Te Amo Meu Amor!! ❤️</h1> 
+  return (
+    <div>
+      <h1>Ana, Eu Te Amo Meu Amor!! ❤️</h1>
 
-                <h1>Apenas mais 1 dia para voce ser oficialmente meia médica 👩‍⚕️⚕️</h1>
-           </div>
+      <h1>Apenas mais 1 dia para voce ser oficialmente meia médica 👩‍⚕️⚕️</h1>
+    </div>
+  );
+}
+
+function Teste() {
+  console.log("teste");
 }
 
 export default Home;
